2b76cf3c5ef4f44361cd8df60e75093f71fb58ea
rahul <rs2570089@gmail.com>
2023-04-10 17:49:02 +0530

    Updated

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -4,6 +4,5 @@ const UserController = require('../controller/usercontroller')
 
 Router.post('/user/register', UserController.register);
 Router.post('/user/login', UserController.login);
-Router.post('/user/logout', UserController.logout);
 
 module.exports = Router;
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:newsapp/homepage.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomePage(email: '',));
+    await tester.pumpWidget(const HomePage(token: '',));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
